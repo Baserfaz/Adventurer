7d9b76995a4d5e8a3ea67dbdae2488912efeb501
Heikki Heiskanen <heikkiheiskanen@hotmail.com>
2017-08-09 19:13:07 +0300

    Added item names as enumerations.

---
+++ Adventurer - Simple Roguelite/src/com/adventurer/enumerations/ItemNames.java
@@ -0,0 +1,27 @@
+package com.adventurer.enumerations;
+
+public enum ItemNames {
+
+	// armor
+	Padded, Leather, StuddedLeather, ChainShirt,
+	Hide, ScaleMail, Chainmail, Breastplate,
+	SplintMail, BandedMail, HalfPlate, FullPlate,
+	
+	// 1h weapons
+	Battleaxe, Club, Flail, Longsword, HeavyMace,
+	Morningstar, HeavyPick, Rapier, Scimitar, Shortspear,
+	BastardSword, Trident, Waraxe, Warhammer, Whip,
+	
+	// light weapons
+	Dagger, LightHammer, Handaxe, LightMace, LightPick,
+	ShortSword, Sickle,
+	
+	// shields
+	Buckler, LightWoodenShield, LightSteelShield, 
+	HeavyWoodenShield, HeavySteelShield, TowerShield,
+	
+	// ranged
+	HandCrossbow, HeavyCrossbow, LightCrossbow,
+	Longbow, ShortBow;
+	
+}
